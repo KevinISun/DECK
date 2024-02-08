97aa5fb8aeb8befd85d0a8a3fbefebad90b64452
Danee Dang <dudang@ucsc.edu>
2024-02-07 16:49:19 -0800

    login page

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:deck_project/firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:deck_project/login_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  runApp(LoginApp());
+}
