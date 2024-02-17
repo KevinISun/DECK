07f54200cf1271ef127a72bb9751d504c2c0c38b
Tommy Chen <tchen175@ucsc.edu>
2024-02-16 17:21:26 -0800

    weather

---
+++ weather_api/server.py
@@ -0,0 +1,18 @@
+from flask import FLASK, request, jsonify
+from weather import get_weather
+
+app = FLASK(__name__)
+
+@app.route('/send_weather_data', methods=['POST'])
+def send_weather_data():
+    city = request.form.get('city')
+    print(city)
+
+    weather_data = get_weather()
+    return jsonify(weather_data)
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
