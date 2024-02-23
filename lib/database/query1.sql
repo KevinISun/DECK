aac7802670208163570d2743be95acf7dbb15952
Tommy Chen <tchen175@ucsc.edu>
2024-02-23 02:53:39 -0800

    some files

---
+++ lib/database/query1.sql
@@ -0,0 +1,23 @@
+INSERT INTO tops(name, color, warmth_level) VALUES
+    ('white t-shirt', 'white', 1),
+    ('black t-shirt', 'black', 1),
+    ('Sweater', 'Red', 4),
+    ('Blouse', 'White', 1);
+
+INSERT INTO bottoms (name, color, warmth_level, bottoms_length, waterproof) VALUES
+    ('Jeans', 'Black', 3, 'long', false),
+    ('Shorts', 'Green', 1, 'short', false),
+    ('Skirt', 'Pink', 2, 'medium', false),
+    ('rainpants', 'Black', 3, 'long', true);
+
+INSERT INTO outerwear (name, color, warmth_level, waterproof) VALUES
+    ('Jacket', 'Black', 3, true),
+    ('Coat', 'Red', 5, true),
+    ('Raincoat', 'Yellow', 3, true);
+
+INSERT INTO shoes (name, color, waterproof) VALUES
+    ('Sneakers', 'White', false),
+    ('Boots', 'Black', true),
+    ('Sandals', 'Brown', false);
+
+
